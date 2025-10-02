e0fd7dad4881f021e12c80643cb96313ce54e419
d-luan <dluan1285@gmail.com>
2025-10-02 09:46:41 -0300

    feat: Create Resource controller, service, and initial DI setup

--- src/AccessibilityHub.WebApp/Dtos/DisabilityDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AccessibilityHub.WebApp.Dtos;
-
-public class DisabilityDto
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string? Description { get; set; }
-}
