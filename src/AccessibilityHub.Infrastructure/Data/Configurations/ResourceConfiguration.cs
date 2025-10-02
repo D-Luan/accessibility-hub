35147ac1bdccc44522d35ebbb003bc8882e20c98
d-luan <dluan1285@gmail.com>
2025-10-02 16:35:47 -0300

    chore(data): Add seed data for Resource entity

---
+++ src/AccessibilityHub.Infrastructure/Data/Configurations/ResourceConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using AccessibilityHub.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class ResourceConfiguration : IEntityTypeConfiguration<Resource>
+{
+    public void Configure(EntityTypeBuilder<Resource> builder)
+    {
+        builder.HasData(
+            new Resource { Id = 1, Name = "Sonova", Description = "...", Url = "https://www.sonova.com/en", Category = "Holding Company", DisabilityId = 1 }
+        );
+    }
+}
