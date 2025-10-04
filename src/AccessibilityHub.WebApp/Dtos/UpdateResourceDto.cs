b63a56ccf68a21df6bb9607568723347b8c249e0
d-luan <dluan1285@gmail.com>
2025-10-04 11:29:12 -0300

    feat(dto): Add UpdateResourceDto for Resource entity

---
+++ src/AccessibilityHub.WebApp/Dtos/UpdateResourceDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccessibilityHub.WebApp.Dtos;
+
+public class UpdateResourceDto
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+
+    [Required]
+    [Url]
+    public string Url { get; set; } = string.Empty;
+
+    [Required]
+    public string Category { get; set; } = string.Empty;
+    public int DisabilityId { get; set; }
+
+}
