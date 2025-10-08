503c64abecf66913ab4e3ed8dfc4ea27762f9429
d-luan <dluan1285@gmail.com>
2025-10-08 19:23:02 -0300

    refactor(Service): Separate interfaces from class implementations

---
+++ src/AccessibilityHub.WebApp/Services/DisabilityService.cs
@@ -0,0 +1,44 @@
+﻿using AccessibilityHub.Infrastructure.Data;
+using AccessibilityHub.WebApp.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccessibilityHub.WebApp.Services;
+
+public class DisabilityService : IDisabilityService
+{
+    private readonly AccessibilityDbContext _context;
+
+    public DisabilityService(AccessibilityDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<DisabilityDto>> GetAllDisabilitiesAsync()
+    {
+        var disabilityDto = await _context.Disabilities
+            .Select(d => new DisabilityDto
+            {
+                Id = d.Id,
+                Name = d.Name,
+                Description = d.Description,
+            })
+            .ToListAsync();
+
+        return disabilityDto;
+    }
+
+    public async Task<DisabilityDto?> GetDisabilityByIdAsync(int id)
+    {
+        var disabilityDto = await _context.Disabilities
+            .Where(d => d.Id == id)
+            .Select(d => new DisabilityDto
+            {
+                Id = d.Id,
+                Name = d.Name,
+                Description = d.Description,
+            })
+            .SingleOrDefaultAsync();
+
+        return disabilityDto;
+    }
+}
--- src/AccessibilityHub.WebApp/Services/IDisabilityService.cs
+++ src/AccessibilityHub.WebApp/Services/IDisabilityService.cs
@@ -9,43 +9,6 @@ namespace AccessibilityHub.WebApp.Services;
 public interface IDisabilityService
 {
     Task<List<DisabilityDto>> GetAllDisabilitiesAsync();
+
     Task<DisabilityDto?> GetDisabilityByIdAsync(int id);
-    public class DisabilityService : IDisabilityService
-    {
-        private readonly AccessibilityDbContext _context;
-
-        public DisabilityService(AccessibilityDbContext context)
-        {
-            _context = context;
-        }
-
-        public async Task<List<DisabilityDto>> GetAllDisabilitiesAsync()
-        {
-            var disabilityDto = await _context.Disabilities
-                .Select(d => new DisabilityDto
-                {
-                    Id = d.Id,
-                    Name = d.Name,
-                    Description = d.Description
-                })
-                .ToListAsync();
-
-            return disabilityDto;
-        }
-
-        public async Task<DisabilityDto?> GetDisabilityByIdAsync(int id)
-        {
-            var disabilityDto = await _context.Disabilities
-                .Where(d => d.Id == id)
-                .Select(d => new DisabilityDto
-                {
-                    Id = d.Id,
-                    Name = d.Name,
-                    Description = d.Description
-                })
-                .SingleOrDefaultAsync();
-
-            return disabilityDto;
-        }
-    }
 }
