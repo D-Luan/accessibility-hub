e3838d2964540ecb8d4e78a5be2521ac8d52e3b0
d-luan <dluan1285@gmail.com>
2025-10-09 16:24:11 -0300

    chore(Data): Add migration to rename Resources table to Providers

---
+++ src/AccessibilityHub.Infrastructure/Migrations/20251009191214_RenameResourceToProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AccessibilityHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameResourceToProvider : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Resources",
+                newName: "Providers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Providers",
+                newName: "Resources");
+        }
+    }
+}
